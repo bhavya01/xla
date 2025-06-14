5862693afa2a61ec12d96772c85901a9c30dd5da
Bhavya Bahl <bbahl@google.com>
2025-06-14 00:46:53 +0000

    Remove infra directory

--- infra/tpu-pytorch-releases/infra_triggers.tf
+++
@@ -1,9 +0,0 @@
-module "terraform_apply" {
-  source = "../terraform_modules/apply_terraform_trigger"
-
-  included_files   = ["infra/**"]
-  branch           = "master"
-  config_directory = "infra/tpu-pytorch-releases"
-
-  worker_pool_id = module.worker_pool.id
-}
--- infra/tpu-pytorch/README.md
+++
@@ -1,5 +0,0 @@
-# Terraform setup for pytorch-xla GCP project
-
-This setup configures:
-* Private docker repository (`docker`).
-* CI test trigger.
